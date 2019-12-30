1c86402ef162a45bfe27125717a82feee48c98a0
Lucas Ferreira <lucas.ferreira@redealumni.com>
2019-12-30 14:49:52 -0300

    fix transactions form

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,6 +3,13 @@ class Transaction < ApplicationRecord
   belongs_to :category
   after_initialize :after_initialize
 
+  validates :account, presence: true
+  validates :category, presence: true
+  validates :value, presence: true
+  validates :date, presence: true
+
+  scope :latest_first, -> { order(date: :desc) }
+
   def after_initialize
     self.date ||= Date.today if new_record?
   end
